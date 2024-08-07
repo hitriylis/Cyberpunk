3a46acca66386efe909c95e3832103a96c33baf2
hitriylis <avlugovov@gmail.com>
2024-08-07 21:12:36 +0400

    Add header and hero section

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    effect: "fade",
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+});
